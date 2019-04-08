053268156bfe30f1f6154db0e640293c3c8f32d6
James.T <torresjames48@yahoo>
2019-04-08 16:17:28 -0500

    base line

---
+++ app/data/friend.js
@@ -0,0 +1,19 @@
+var friendsArray = [{
+
+    name : "James",
+    photo : "",
+    score : [
+            '1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+    ]
+}];
+
+module.exports = friendsArray;
