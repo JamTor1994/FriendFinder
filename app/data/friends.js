ea0341c541fa471b18200562c1d5b4a5073b27d1
James.T <torresjames48@yahoo>
2019-04-09 14:46:06 -0500

    css updates a little polisshin completed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,21 @@
 var friendsArray = [{
 
-    name : "James",
-    photo : "",
+    name : "Kujo Jotaro",
+    photo : "https://tse2.mm.bing.net/th?id=OIP.CJpTf-MnT-chCAfwYbS_YwHaKj&pid=Api&P=0&w=300&h=300",
+    scores : [
+            '1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+	],
+	name : "Goku",
+    photo : "https://tse4.mm.bing.net/th?id=OIP.F78f0vA9IMPmI3tQyMnfIAHaHy&pid=Api&P=0&w=300&h=300",
     scores : [
             '1',
 			'5',
@@ -13,7 +27,35 @@ var friendsArray = [{
 			'1',
 			'1',
 			'5'
-    ]
+	],
+	name : "Joseph Joe Star",
+    photo : "https://tse3.explicit.bing.net/th?id=OIP.q3XX_65bRJQ4L7mqlQYtNwHaJj&pid=Api&P=0&w=300&h=300",
+    scores : [
+            '5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+	],
+	name : "Johnathen Joe Star",
+    photo : "https://pbs.twimg.com/profile_images/826268879345311746/cu3GjfiO.jpg",
+    scores : [
+            '1',
+			'3',
+			'4',
+			'3',
+			'4',
+			'2',
+			'5',
+			'1',
+			'3',
+			'5'
+    ],
 }];
 
 module.exports = friendsArray;
